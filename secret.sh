a0fdf4ecad7edf41facb5363529b825f07fac142
Steve Hay <github@stevenhay.com>
2025-03-03 22:55:47 -0500

    uses asymmetric encryption for ease of use

--- secret.sh
+++ secret.sh
@@ -1,47 +1,66 @@
-secret() (
-  secrets_dir="$HOME"/.secrets
+secret()
+(
+    secrets_dir="$HOME"/.secrets
 
-  encrypt() { gpg -c --output "$1"; }
-  decrypt() { gpg --quiet --batch --decrypt "$1"; }
-  hash()    { printf "%s" "$1" | openssl dgst -sha512 | awk '{print $2}'; }
+    name()    { printf "%s" "$(whoami)"; }
+    email()   { printf "%s" "secret@$(hostname -d)"; }
+    encrypt() { gpg --quiet --encrypt --recipient "$(email)" --output "$1"; }
+    decrypt() { gpg --quiet --decrypt "$1"; }
+    hash()    { printf "%s" "$1" | openssl dgst -sha512 | awk '{print $2}'; }
 
-  salt() {
-    local saltfile="${secrets_dir}/salt"
-    if [[ ! -f "$saltfile" ]]
-    then
-      openssl rand -hex 32 | encrypt "$saltfile"
-    fi
-    decrypt "$saltfile"
-  }
-
-    
-  mkdir -p "$secrets_dir"
-  chmod 700 "$secrets_dir"
-  if [ $# -eq 2 ]
-  then
-    keyfile="${secrets_dir}/$(hash "$2$(salt)").gpg"
-    case "$1" in
-      get)
-        if [[ -f "$keyfile" ]]
+    genkey()
+    { 
+        if ! gpg --list-keys | grep --quiet "$(email)"
         then
-          decrypt "$keyfile"
-        else
-          >&2 echo "Key not found."
-          exit 1
+            local fingerprint
+            gpg --batch --quick-generate-key "$(name) <$(email)>" rsa4096 default 0
+            fingerprint=$(gpg --list-secret-keys --with-colons "$(email)" | grep '^fpr' | head -n1 | awk -F: '/^fpr/ {print $10}' )
+            gpg --batch --quick-add-key "$fingerprint" rsa4096 encrypt 0
         fi
-        ;;
-      set)
-        read -s -p "Enter secret value: " secret
-        printf "%s" "$secret" | encrypt "$keyfile"
-        ;;
-      *)
-        >&2 echo "Invalid command: $1"
-        exit 1
-        ;;
-    esac
-  else
-    >&2 echo "Usage: $0 set|get key"
-    exit 1
-  fi    
-)
+    }
 
+    salt() 
+    {
+        local saltfile="${secrets_dir}/salt"
+        if [[ ! -f "$saltfile" ]]
+        then
+            openssl rand -hex 32 | encrypt "$saltfile"
+        fi
+        decrypt "$saltfile"
+    }
+
+    initialize()
+    {
+        mkdir -p  "$secrets_dir"
+        chmod 700 "$secrets_dir"
+        genkey
+    }
+    
+    initialize
+    if [ $# -eq 2 ]
+    then
+        keyfile="${secrets_dir}/$(hash "$2$(salt)").gpg"
+        case "$1" in
+            get)
+                if [[ -f "$keyfile" ]]
+                then
+                    decrypt "$keyfile"
+                else
+                    >&2 echo "Key not found."
+                    exit 1
+                fi
+                ;;
+            set)
+                read -r -s -p "Enter secret value: " secret
+                printf "%s" "$secret" | encrypt "$keyfile"
+                ;;
+            *)
+                >&2 echo "Invalid command: $1"
+                exit 1
+                ;;
+        esac
+    else
+        >&2 echo "Usage: $0 set|get key"
+        exit 1
+    fi    
+)
